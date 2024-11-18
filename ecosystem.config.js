d4bb7fb528659abffdb1dd0a94bfc32c045726e3
vladlentsoy <vladlengg@gmail.com>
2024-11-18 05:20:49 +0500

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
+module.exports = {
+  apps: [{
+    name: process.env.APP_NAME, // Имя приложения
+    script: 'npm', // Используем npm для запуска
+    args: 'start', // Аргумент для npm (запуск команды `npm start`)
+    cwd: './', // Рабочая директория (корень проекта Strapi)
+    instances: 1, // Количество инстансов (1 для разработки, может быть больше для продакшена)
+    autorestart: true, // Автоматический перезапуск при сбоях
+    watch: false, // Отключение наблюдения за файлами (включите в dev-режиме)
+    max_memory_restart: '1G', // Перезапуск при превышении 1 ГБ памяти
+  },],
+};
